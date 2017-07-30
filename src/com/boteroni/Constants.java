21a8d776b7a97e1eb366ce9b812e4562acaade06
Matt Nguyen <Matt Nguyen>
2017-07-30 14:11:15 -0700

    First commit, has pong code + library

---
+++ src/com/boteroni/Constants.java
@@ -0,0 +1,9 @@
+package com.boteroni;
+
+/**
+ * Created by skips on 7/30/2017.
+ */
+public class Constants {
+    public static String BOT_TOKEN = "MzQxMDczMDY0NDU0MjU4Njg4.DF-WVw.7GUmOzaIF7QweXnQIBN_i40WAO8";
+
+}
---
+++ src/com/boteroni/Main.java
@@ -0,0 +1,27 @@
+package com.boteroni;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) {
+        JDA discord = null;
+
+        try {
+            discord = new JDABuilder(AccountType.BOT)
+                    .setToken(Constants.BOT_TOKEN)
+                    .buildBlocking();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RateLimitedException e) {
+            e.printStackTrace();
+        }
+        discord.addEventListener(new Messenger());
+    }
+}
